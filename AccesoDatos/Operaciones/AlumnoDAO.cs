e70b6f872f115e2cca27abdbbb66ddb5200e8fea
Javi Llinares <javierllinaresbarral@outlook.com>
2024-02-07 16:11:27 +0100

    Añadido un proyecto de Consola para realizar pruebas. Posteriormente lo eliminaré. Funcionalidades básicas.

---
+++ AccesoDatos/Operaciones/AlumnoDAO.cs
@@ -0,0 +1,16 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+
+namespace AccesoDatos.Operaciones
+{
+    public class AlumnoDAO
+    {
+        public GestorAlumnosContext _context = new GestorAlumnosContext();
+
+        public List<Alumno> SeleccionarTodos()
+        {
+            var alumnos = _context.Alumnos.ToList();
+            return alumnos;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using AccesoDatos.Operaciones;
+
+AlumnoDAO opAlumno = new AlumnoDAO();
+var alumnos = opAlumno.SeleccionarTodos();
+
+foreach (var alumno in alumnos)
+{
+    Console.WriteLine(alumno.Nombre);
+}
