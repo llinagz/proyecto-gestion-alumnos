2d96b43076203f171dc25a4033a651f0d8956b75
Javi Llinares <javierllinaresbarral@outlook.com>
2024-02-09 12:32:53 +0100

    Creacion inicial de la API y comunicacion con el proyecto de acceso a los datos en la BBDD

---
+++ AccesoDatos/Operaciones/ProfesorDAO.cs
@@ -0,0 +1,16 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+
+namespace AccesoDatos.Operaciones
+{
+    public class ProfesorDAO
+    {
+        public GestorAlumnosContext _context = new GestorAlumnosContext();
+
+        public Profesor Login(string usuario, string pass)
+        {
+            var prof = _context.Profesors.Where(p => p.Usuario == usuario && p.Pass == pass).FirstOrDefault();
+            return prof;
+        }
+    }
+}
---
+++ WebApi/Controllers/ProfesorController.cs
@@ -0,0 +1,27 @@
+﻿using AccesoDatos.Models;
+using AccesoDatos.Operaciones;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        ProfesorDAO profesorDAO = new ProfesorDAO();
+
+        [HttpPost("autenticacion")]
+        public string Login([FromBody] Profesor prof)
+        {
+            var profesor = profesorDAO.Login(prof.Usuario, prof.Pass);
+            if(profesor != null)
+            {
+                return profesor.Usuario;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
